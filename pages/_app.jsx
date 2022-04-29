43af4d04dd9daae755779eca59e225a2f5e3ffa1
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-04-29 00:20:59 -0300

    chore: add `jit` to tailwind config

---
+++ pages/_app.jsx
@@ -0,0 +1,11 @@
+import "../styles/globals.css";
+
+function MyApp() {
+  return (
+    <div className="text-3xl">
+      <h1>Star Wars wiki</h1>
+    </div>
+  );
+}
+
+export default MyApp;
--- src/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import "../styles/globals.css";
-
-function MyApp() {
-  return <h1>Star Wars wiki</h1>;
-}
-
-export default MyApp;
