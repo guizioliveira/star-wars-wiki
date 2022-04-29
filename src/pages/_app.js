807bbe2e2a212971f9f17111c0a8cca0ec3b1443
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-04-28 23:42:36 -0300

    chore: next `pages` structure

---
+++ src/pages/_app.js
@@ -0,0 +1,7 @@
+import "../styles/globals.css";
+
+function MyApp() {
+  return <h1>Star Wars wiki</h1>;
+}
+
+export default MyApp;
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Star Wars wiki</title>
+        <meta
+          name="description"
+          content="A wiki to learn more about characters and planets of the Star Wars world "
+        />
+      </Head>
+    </div>
+  );
+}
