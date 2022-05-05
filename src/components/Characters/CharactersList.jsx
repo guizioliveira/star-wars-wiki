0425f4d93994c5f9ce15eedd565175e650f0b657
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-05-05 20:51:25 -0300

    refactor(components): move character state

--- src/components/Characters/CharactersList.jsx
+++ src/components/Characters/CharactersList.jsx
@@ -1,11 +1,10 @@
 /* eslint-disable import/prefer-default-export */
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Loading } from "../Loading";
 import { getDataFromMultiplePagesAPI } from "../../pages/api/characterAPI";
 import { CharactersCard } from "./CharactersCard";
 
-export function CharacterList() {
-  const [characters, setCharacters] = useState([]);
+export function CharacterList({ charData, setCharacters }) {
   useEffect(() => {
     getDataFromMultiplePagesAPI().then((data) => {
       setCharacters(data);
@@ -14,11 +13,13 @@ export function CharacterList() {
 
   return (
     <>
-      {characters.length === 0 ? (
-        <Loading />
+      {charData.length === 0 ? (
+        <div className="pt-10">
+          <Loading />
+        </div>
       ) : (
         <div className="py-4 w-full grid grid-cols-1 xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-4 xl:gap-9">
-          {characters
+          {charData
             ?.sort((a, b) => a.name.localeCompare(b.name))
             .map((char, index) => (
               <CharactersCard key={index} details={char} />
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import { CharacterList } from "../components/Characters/CharactersList";
 import { Header } from "../components/Header";
 import "../styles/globals.css";
 
 function MyApp() {
+  const [characters, setCharacters] = useState([]);
   return (
     <div className="container mx-auto">
       <Header />
-      <CharacterList />
+      <CharacterList charData={characters} setCharacters={setCharacters} />
     </div>
   );
 }
