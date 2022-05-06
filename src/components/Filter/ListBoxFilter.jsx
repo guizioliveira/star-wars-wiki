200197cf28979cef5ae20c612115a31adf5e40d6
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-05-06 00:12:26 -0300

    feat(components): create and use `Filter`

---
+++ src/components/Filter/ListBoxFilter.jsx
@@ -0,0 +1,59 @@
+import { Fragment } from "react";
+import { Transition, Listbox } from "@headlessui/react";
+import { Check, CaretDown } from "phosphor-react";
+
+function capitalize(string) {
+  return string.replace(/^\w/, (c) => c.toUpperCase());
+}
+
+export function ListBoxFilter({ optionsList, getter, setter }) {
+  return (
+    <Listbox value={getter} onChange={setter}>
+      <div className="relative mt-1">
+        <Listbox.Button className="relative w-full cursor-pointer rounded-lg bg-primary-900 py-2 pl-3 pr-10 text-left focus:outline-nones focus-visible:ring-2 focus-visible:ring-primary-900 focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-primary-500 sm:text-sm text-white">
+          <span className="block truncate">{capitalize(getter.name)}</span>
+          <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+            <CaretDown className="h-5 w-5 text-gray-400" aria-hidden="true" />
+          </span>
+        </Listbox.Button>
+        <Transition
+          as={Fragment}
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-primary-900 py-1 text-white text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            {optionsList.map((option) => (
+              <Listbox.Option
+                key={option.id}
+                className={({ active }) =>
+                  `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                    active && "bg-primary-500"
+                  }`
+                }
+                value={option}
+              >
+                {({ selected }) => (
+                  <>
+                    <span
+                      className={`block truncate ${
+                        selected ? "font-medium" : "font-normal"
+                      }`}
+                    >
+                      {capitalize(option.name)}
+                    </span>
+                    {selected && (
+                      <span className="absolute inset-y-0 left-0 flex items-center pl-3 ">
+                        <Check className="h-5 w-5" aria-hidden="true" />
+                      </span>
+                    )}
+                  </>
+                )}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </Transition>
+      </div>
+    </Listbox>
+  );
+}
---
+++ src/components/Filter/index.jsx
@@ -0,0 +1,122 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { useState, Fragment } from "react";
+import { MagnifyingGlass, Funnel, X } from "phosphor-react";
+import { ListBoxFilter } from "./ListBoxFilter";
+
+const filters = [
+  { id: 1, name: "name" },
+  { id: 2, name: "birth" },
+  { id: 3, name: "height" },
+  { id: 4, name: "mass" }
+];
+
+const order = [
+  { id: 1, name: "ascending" },
+  { id: 2, name: "descending" }
+];
+
+export function Filter() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [filterBy, setFilterBy] = useState(filters[0]);
+  const [orderBy, setOrderBy] = useState(order[0]);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function handleFilters() {
+    console.log({ filterBy, orderBy });
+    closeModal();
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+  return (
+    <div className="w-full flex mt-3 sm:mt-0 justify-end">
+      <div className="relative w-3/4 sm:w-[225px] flex items-center text-gray-400 focus-within:text-white group">
+        <MagnifyingGlass
+          weight="bold"
+          className="absolute w-[18px] h-[18px] left-3 pointer-events-none"
+        />
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-primary-900 py-2 px-2 pl-9 text-sm rounded-2xl w-full"
+        />
+      </div>
+      <button
+        onClick={openModal}
+        className="bg-primary-900 w-1/4 h-9 sm:w-9 ml-3 rounded-full flex justify-center items-center text-gray-400 hover:text-white hover:bg-primary-500 transition"
+      >
+        <Funnel weight="bold" className="w-[18px] h-[18px]" />
+      </button>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform rounded-2xl bg-gradient-to-b from-primary-900 to-dark-800 p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h2"
+                    className="text-2xl font-medium text-white"
+                  >
+                    Filters
+                  </Dialog.Title>
+                  <button
+                    onClick={closeModal}
+                    className="absolute top-6 right-6 text-white hover:text-primary-500"
+                  >
+                    <X weight="bold" className="w-6 h-6" />
+                  </button>
+                  <div className="mt-2 flex flex-col gap-2">
+                    <ListBoxFilter
+                      optionsList={filters}
+                      getter={filterBy}
+                      setter={setFilterBy}
+                    />
+                    <ListBoxFilter
+                      optionsList={order}
+                      getter={orderBy}
+                      setter={setOrderBy}
+                    />
+                  </div>
+
+                  <div className="mt-4">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md bg-primary-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={handleFilters}
+                    >
+                      Apply Filters
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </div>
+  );
+}
