a9530b5720d3ca6e4cb7390f40ce06662370fbbf
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-05-07 04:41:30 -0300

    feat(components): increase overlay opacity on `Popup` component

--- src/components/Commons/Popup.jsx
+++ src/components/Commons/Popup.jsx
@@ -19,7 +19,7 @@ export function Popup(props) {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-black bg-opacity-25" />
+          <div className="fixed inset-0 bg-black bg-opacity-75" />
         </Transition.Child>
         <div className="fixed inset-0 overflow-y-auto">
           <div className="flex min-h-full items-center justify-center text-center">
